0e8ae60d227fa55de75ec30ee88b7b6a9bb5db29
eru <eru0@pm.me>
2019-03-03 17:01:48 +0100

    .

--- netstatcount.sh
+++ netstatcount.sh
@@ -2,14 +2,14 @@
 # Script to monitor the tcp sockets
 
 ALL=0
-PORT=""
 REFRESH=1
 STATUSES=( )
+PORTS=( "" )
 while getopts p:r:as: option
     do
         case "${option}"
         in
-			p) PORT=":${OPTARG}";;
+			p) PORTS=("${PORTS[@]}" ":${OPTARG}");;
 			a) ALL=1;;
 			r) REFRESH=${OPTARG};;
 			s) STATUSES=("${STATUSES[@]}" "${OPTARG}");;
@@ -18,12 +18,16 @@ done
 
 [ ${#STATUSES[@]} -eq 0 ] && STATUSES=( TIME_WAIT ESTABLISHED )
 [ $ALL -eq 1 ] && STATUSES=( TIME_WAIT ESTABLISHED LISTEN SYN-SENT SYN-RECEIVED FIN-WAIT-1 FIN-WAIT-2 CLOSE-WAIT CLOSING LAST-ACK CLOSED )
-while true; do 
-	NS=$(netstat -nat); [[ "$PORT" ]] && NS=$(echo "$NS" | grep $PORT) 
+while true; do
+	NS=$(netstat -nat);
 	clear
-	for STATUS in "${STATUSES[@]}"
+	for PORT in "${PORTS[@]}"
 	do
-		echo "$STATUS $PORT" ; echo "$NS" | grep $STATUS | wc -l
+		[[ "$PORT" ]] && NS=$(echo "$NS" | grep $PORT) 
+		for STATUS in "${STATUSES[@]}"
+		do
+			echo "$STATUS $PORT" ; echo "$NS" | grep $STATUS | wc -l
+		done
 	done
 	echo "----"
 	echo "TOTAL"
