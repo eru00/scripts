430a717f17d0c98b28d6653a86f1214f6335b996
eru <eru0@pm.me>
2019-03-03 16:52:33 +0100

    .

--- netstatcount.sh
+++ netstatcount.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 # Script to monitor the tcp sockets
 
+ALL=0
+PORT=""
+REFRESH=1
+STATUSES=( )
+while getopts p:r:as: option
+    do
+        case "${option}"
+            in
+                p) PORT=":${OPTARG}";;
+                a) ALL=1;;
+				r) REFRESH=${OPTARG};;
+				s) STATUSES=("${STATUSES[@]}" "${OPTARG}")
+        esac
+done
+
+[ ${#STATUSES[@]} -eq 0 ] && STATUSES=( TIME_WAIT ESTABLISHED )
+[ $ALL -eq 1 ] && STATUSES=( TIME_WAIT ESTABLISHED LISTEN SYN-SENT SYN-RECEIVED FIN-WAIT-1 FIN-WAIT-2 CLOSE-WAIT CLOSING LAST-ACK CLOSED )
 while true; do 
-	NS=$(netstat -nat)
+	NS=$(netstat -nat); [[ "$PORT" ]] && NS=$(echo "$NS" | grep $PORT) 
 	clear
-	echo "TIME_WAIT"
-	echo "$NS" | grep TIME_WAIT | wc -l
-	echo "ESTABLISHED"
-	echo "$NS" | grep ESTABLISHED | wc -l
+	for STATUS in "${STATUSES[@]}"
+	do
+		echo "$STATUS $PORT" ; echo "$NS" | grep $STATUS | wc -l
+	done
 	echo "----"
 	echo "TOTAL"
 	echo "$NS" | wc -l
-	sleep 0.6
+	sleep $REFRESH
 done
